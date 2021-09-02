798be87e18376fbb10c6866830b1b808e30df93d
Duncan <duncan@dinegigs.com>
2021-09-02 14:22:38 -0400

    Added href to index.html

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -5,14 +5,14 @@ import styles from './NavBar.module.css';
 const NavBar = () => (
   <div className={styles.NavBar} data-testid="NavBar">
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <a className="navbar-brand" href="/">BadBank</a>
+      <a className="navbar-brand" href="index.html#/">BadBank</a>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
       <div className="collapse navbar-collapse" id="navbarNav">
         <ul className="navbar-nav">
         <li className="nav-item">
-            <a className="nav-link" href="/" title="Or just hit the BadBank logo">Home</a>
+            <a className="nav-link" href="index.html#/" title="Or just hit the BadBank logo">Home</a>
           </li> 
           <li className="nav-item">
             <a className="nav-link" href="#/CreateAccount/" title="I wouldn't do this if I were you.">Create Account</a>
